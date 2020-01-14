079877a021aedfd725be33e932d50b96f94df254
xgf <xgf_aon@163.com>
2020-01-14 10:31:26 +0800

    随机背景图tool

--- main_app/tool/bp_tool.py
+++ main_app/tool/bp_tool.py
@@ -4,13 +4,17 @@ Description: 工具接口
 Date: 2019-12-25
 Author: xgf
 """
+import random
 import time
+
+import requests
 import ujson as json
 
 from flask import Blueprint, current_app
 
 from lib.utils import create_qrcode
 from lib._flask import request_args
+from models import Images
 
 bp = Blueprint('bp_tool', __name__)
 
@@ -30,3 +34,15 @@ def wx_arcode():
         return json.dumps({'code': 1001, 'message': '成功', 'datas': img_url})
     else:
         return json.dumps({'code': 1000, 'message': '失败'})
+
+
+@bp.route('/random/bg')
+def random_bg():
+    """
+        随机背景图
+    """
+    images = Images.query.filter_by(is_use=True).all()
+    images = [i.to_list() for i in images]
+    bg = random.choice(images)['name']
+
+    return json.dumps({'code': 1001, 'message': '成功', 'datas': bg})
