eef72fd5d463e6c55142d88d551ad032ef0f6f21
xgf <xgf_aon@163.com>
2020-05-13 17:16:38 +0800

    add: 一键美颜

--- main_app/tool/bp_tool.py
+++ main_app/tool/bp_tool.py
@@ -8,15 +8,15 @@ import os
 import random
 import time
 import traceback
-
 import requests
 import ujson as json
 
-from flask import Blueprint, current_app, request
+from flask import Blueprint, current_app, request, render_template, redirect, url_for, flash
 from flask.views import MethodView
 
 from lib.utils import create_qrcode
-from lib._flask import request_args
+from lib import utils
+from lib._flask import request_args, request_form
 from models import Images
 from lib._qiniu import upload_file, fill_domain, upload_data
 
@@ -68,4 +68,30 @@ class EditorUpload(MethodView):
         return json.dumps({'success': 1, 'message': '上传成功', 'url': url})
 
 
+class Beauty(MethodView):
+    """美颜工具"""
+    def get(self):
+        
+        return render_template('tool/beauty.html')
+
+    def post(self):
+        level = request_form('level', type=int, default=28)
+        file = request.files.get('image')
+        print(file)
+        if file and utils.allowed_file(file.filename):
+            path = './main_app/static/images/beauty/{}'.format(utils.get_filename(file.filename))
+            file.save(path)  # 保存上传的图片
+            newpath = utils.beauty(path, file.filename, value=level)  # 生成新图片的路径
+            img_url = fill_domain(upload_file(file.filename, newpath))
+            print("生成的", img_url)
+            message={"message": "上传成功", "category": "info"}
+
+            return render_template('tool/beauty.html', img_url=img_url, message=message)
+        else:
+            message={"message": "文件类型必须是图片！", "category": "danger"}
+
+            return render_template('tool/beauty.html', message=message)
+
+
 bp.add_url_rule('/editor/upload', view_func=EditorUpload.as_view('editor_upload'))
+bp.add_url_rule('/beauty', view_func=Beauty.as_view('beauty'))
